074f5e82d26e0fba9840e8b6cca0a37f11cafd94
wrjang96 <66419215+wrjang96@users.noreply.github.com>
2021-08-09 19:46:46 +0900

    21

---
+++ src/test/java/com/example/WebProject/WebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
