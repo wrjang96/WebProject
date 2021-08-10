e9e3d1a5f76bcdc6da3df2e07671a32108785c7c
wrjang96 <66419215+wrjang96@users.noreply.github.com>
2021-08-10 20:30:04 +0900

    Add H2 database

---
+++ src/main/java/com/example/WebProject/HelloController.java
@@ -0,0 +1,4 @@
+package com.example.WebProject;
+
+public class HelloController {
+}
--- src/main/java/com/example/WebProject/WebProjectApplication.java
+++ src/main/java/com/example/WebProject/WebProjectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebProjectApplication {
+public class WebProjectApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebProjectApplication.class, args);
